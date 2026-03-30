9cb845801982cc9824473ef6cb8e7955544eed0d
zarrukh <z.jurakulov@uzinfocom.uz>
2026-03-30 15:40:44 +0500

    changes

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,9 +1,26 @@
 import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
-  async findOne(id: string) {}
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
 
-  async findAll() {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
 
-  async create(message: string) {}
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content: content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
