c727b1a5f55525a27a415c2a75421847ae218aa7
zarrukh_jurakulov <zarrukhjurakulov474@gmail.com>
2026-03-31 23:47:54 +0500

    changes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -23,7 +23,13 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async  getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if(!message) {
+      return new NotFoundException("Message not found")
+    }
+
+    return message;
   }
 }
